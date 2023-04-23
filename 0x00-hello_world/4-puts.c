6996700470db60456ff92e2c701b2832e5b8ad9e
ElsieShayne <elsieshayne2@gmail.com>
2023-04-23 06:04:07 -0700

    Hello puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *return - always zero means success
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
