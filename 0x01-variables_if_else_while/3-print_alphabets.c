7b8e86e57f63001e4c3f9ffbc288e0262d464a3b
ElsieShayne <elsieshayne2@gmail.com>
2023-04-24 10:39:00 -0700

    Lower and uppercase task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - this program prints the alphabets in lower and uppercase.
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+
+	int n = 97;
+	int y = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+
+	while (y <= 90)
+	{
+		putchar(y);
+		y++;
+	}
+
+	putchar('\n');
+	return (0);
+}
