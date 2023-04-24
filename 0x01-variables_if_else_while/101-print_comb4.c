d25e9fe13edf2e174c0d6e20cf5646ebd1c05ec5
ElsieShayne <elsieshayne2@gmail.com>
2023-04-24 15:40:43 -0700

    Do what you do better task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - this program will print 3 combinations of numbers.
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 48; x < 58; x++)
+	{
+		for (y = 49; y < 58; y++)
+		{
+			for (z = 50; z < 58; z++)
+			{
+				if (z > y && y > x)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+
+					if (x != 55 || y != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
