ded832c9714b032aa740fa9115c75d5e1d64d5da
ElsieShayne <elsieshayne2@gmail.com>
2023-04-24 11:32:02 -0700

    Base 10 numbers task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - this program prints all single digit base 10 numbers.
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+
+	int y;
+
+	while (y <= 9)
+	{
+		printf("%d", y);
+			y++;
+	}
+
+	printf("\n");
+	return (0);
+}
