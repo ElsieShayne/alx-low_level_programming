b2106dd76fe4ad6dbf997c31cf89f030ab8a48c4
ElsieShayne <elsieshayne2@gmail.com>
2023-04-26 15:59:42 -0700

    Addition task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - main entry point. Adds two integers.
+ *
+ * @x: first integer
+ * @y: second integer
+ * Return: the integer value of result.
+*/
+int add(int x, int y)
+{
+	return (x + y);
+}
