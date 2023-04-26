b9fd722d6a6198839acb4e07d352532291db6b48
ElsieShayne <elsieshayne2@gmail.com>
2023-04-26 09:01:08 -0700

    Alphabet lowercase task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - this program prints the alphabets in lowercase.
+ *
+ * Return: Always 0 (success)
+*/
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+
+	return (0);
+}
