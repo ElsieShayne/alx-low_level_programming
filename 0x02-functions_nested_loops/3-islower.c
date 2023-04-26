5d7be670b7b6cd3acfa9b37fe88d86aa97d24167
ElsieShayne <elsieshayne2@gmail.com>
2023-04-26 11:42:36 -0700

    Lowercase task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - main entry. It shows 1, if input is lowercase and 0 if otherwise.
+ *
+ * @c: integer representing the ASCII code to check.
+ * Return: 1 for lowercase character, 0 for otherwise.
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		_putchar('\n');
+		return (1);
+	}
+	else
+	{
+		_putchar('\n')
+		return (0);
+	}
+}
