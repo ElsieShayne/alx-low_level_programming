3518213ed80399942fc98cc37aeb4d3fbbac5e7a
ElsieShayne <elsieshayne2@gmail.com>
2023-04-26 16:46:22 -0700

    98 numbers task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Main entry point. Prints all natural numbers from n to 98
+ * @n: number to start printing from
+ *
+ * Return: Always 0
+*/
+void print_to_98(int n)
+{
+	for (int i = n; i <= 98; i++)
+	{
+		printf("%d\n", i);
+	}
+}
